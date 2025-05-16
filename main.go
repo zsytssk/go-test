ba9717d0310a354c470ae3d1a71ba60cf847dc2a
zsytssk <zsytssk@gmail.com>
2025-05-16 14:07:03 +0800

    [master]

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"sort"
-
-	fuzzy "github.com/paul-mannino/go-fuzzywuzzy"
+	"os"
+	"path/filepath"
 )
 
 type Candidate struct {
@@ -14,23 +12,10 @@ type Candidate struct {
 }
 
 func main() {
-	input := "fzr"
-	candidates := []Candidate{
-		{"fzf", 1.5, 0},
-		{"fuzzy", 1.0, 0},
-		{"foo", 0.5, 0},
+	ex, err := os.Executable()
+	if err != nil {
+		return
 	}
-
-	for i := range candidates {
-		candidates[i].Score = fuzzy.Ratio(input, candidates[i].Text)
-		candidates[i].Score = int(float64(candidates[i].Score) * candidates[i].Weight)
-	}
-
-	sort.Slice(candidates, func(i, j int) bool {
-		return candidates[i].Score > candidates[j].Score
-	})
-
-	for _, c := range candidates {
-		fmt.Printf("%s (score: %d)\n", c.Text, c.Score)
-	}
+	basename := filepath.Base(ex)
+	println(basename)
 }
